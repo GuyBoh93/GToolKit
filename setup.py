2f2635143c1f429aa87c44ab8459fc2d6980f966
GuyBoh93 <65979080+GuyBoh93@users.noreply.github.com>
2021-01-30 10:15:15 +0000

    added install reqierments

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    
 setuptools.setup(
     name="GToolKit", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
+    install_requires=requirements,
 )
